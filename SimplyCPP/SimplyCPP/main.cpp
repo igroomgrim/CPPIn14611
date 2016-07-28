356ceea5c9ee0b744433b0c0e6c29a905304d92f
Anak Mirasing <thaihooligan@hotmail.com>
2016-07-29 01:55:50 +0700

    Initial CPP project

---
+++ SimplyCPP/SimplyCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimplyCPP
+//
+//  Created by Anak Mirasing on 7/29/2559 BE.
+//  Copyright © 2559 iGROOMGRiM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
